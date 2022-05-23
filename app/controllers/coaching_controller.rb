a4333b70da0cfc7bf3d24642192b233eef3a4dd0
bruna suzuki <brunasuzuki12@gmail.com>
2022-05-23 16:37:43 -0700

    rails new

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,6 +1,18 @@
 class CoachingController < ApplicationController
+
+  def ask
+  end
+
   def answer
-    @question = great
-    @answer = coach_answer(@question)
+    @question = params[:question]
+
+    if @question.downcase == 'i am going to work'
+      @answer = 'Great'
+
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
